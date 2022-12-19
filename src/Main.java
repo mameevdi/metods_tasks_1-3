32a0bd272d771660ffffb737d37a426ff5573fee
mameevdi <mameevs@gmail.com>
2022-12-20 00:36:53 +0300

    Закончил 1-ю задачу из 3-х

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+    public static void year(int currentYear) {
+        if (((currentYear % 4 == 0) && (currentYear % 100 != 0)) || (currentYear % 400 == 0)) {
+            System.out.println(currentYear + " это високосный год");
+        } else {
+            System.out.println(currentYear + " это невисокосный год");
+        }
+    }
     public static void main(String[] args) {
-
+        year(2023);
     }
 }
